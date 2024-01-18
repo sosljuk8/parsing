72a132fa1768cec29bb4a42e5d7318cd413d576a
estatie <estatie@proton.me>
2024-01-19 01:40:20 +0700

    Migrate command controller added.

--- cmd.go
+++ cmd.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/spf13/cobra"
+	"log/slog"
+	"parsing/orm"
 )
 
 // RootCmd represents the base command when called without any subcommands
@@ -9,4 +11,17 @@ var rootCmd = &cobra.Command{
 	Use:   "home",
 	Short: "A brief description of your application",
 	Long:  `A longer description that spans multiple lines`,
+}
+
+var migrateCmd = &cobra.Command{
+	Use:   "migrate",
+	Short: "Migrate database schema",
+	Long:  `Note this command must not be run in parallel with processing commands`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		err := orm.Migrate()
+		if err != nil {
+			slog.Error("Migrate", err)
+		}
+	},
 }
