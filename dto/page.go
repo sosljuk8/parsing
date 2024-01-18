89fbd21b60c37a6172367817ae32957414130093
estatie <estatie@proton.me>
2024-01-19 02:40:45 +0700

    Remove Pages collection until safe concurrency required.

--- dto/page.go
+++ dto/page.go
@@ -1,7 +1,6 @@
 package dto
 
 import (
-	"sync"
 	"time"
 )
 
@@ -14,55 +13,4 @@ type Page struct {
 	Created   time.Time
 	Updated   time.Time
 	Processed time.Time
-}
-
-// Pages is not safe for concurrent use collection of Page based on map with URL as a key
-type Pages struct {
-	items map[string]*Page
-	mux   sync.RWMutex
-}
-
-// NewPages creates new Pages
-func NewPages() *Pages {
-	return &Pages{
-		items: make(map[string]*Page),
-		mux:   sync.RWMutex{},
-	}
-}
-
-// Add adds page to collection
-func (p *Pages) Add(page *Page) {
-	p.mux.Lock()
-	defer p.mux.Unlock()
-	p.items[page.URL] = page
-
-}
-
-// All returns all pages
-func (p *Pages) All() []*Page {
-	var pages []*Page
-	for _, page := range p.items {
-		pages = append(pages, page)
-	}
-	return pages
-}
-
-// Len returns number of pages
-func (p *Pages) Len() int {
-	return len(p.items)
-}
-
-// URLs returns all urls
-func (p *Pages) URLs() []string {
-	var urls []string
-	for url := range p.items {
-		urls = append(urls, url)
-	}
-	return urls
-}
-
-func (p *Pages) Remove(url string) {
-	p.mux.Lock()
-	defer p.mux.Unlock()
-	delete(p.items, url)
 }
--- dto/page_test.go
+++ dto/page_test.go
@@ -2,43 +2,9 @@ package dto_test
 
 import (
 	"github.com/brianvoe/gofakeit/v6"
-	"github.com/stretchr/testify/assert"
 	"parsing/dto"
 	"testing"
 )
-
-func TestNewPages(t *testing.T) {
-
-	// create empty pages
-	pages := dto.NewPages()
-	count := 0
-
-	// add 10 pages
-	for i := 1; i <= 10; i++ {
-
-		// add page
-		pages.Add(NewPage(t))
-		count++
-
-		// check by length
-		assert.Equal(t, count, pages.Len())
-		assert.Equal(t, count, len(pages.URLs()))
-		assert.Equal(t, count, len(pages.All()))
-	}
-
-	// remove 5 pages
-	for i := 1; i <= 5; i++ {
-
-		// remove page
-		pages.Remove(pages.All()[0].URL)
-		count--
-
-		// check by length
-		assert.Equal(t, count, pages.Len())
-		assert.Equal(t, count, len(pages.URLs()))
-		assert.Equal(t, count, len(pages.All()))
-	}
-}
 
 func NewPage(t *testing.T) *dto.Page {
 	return &dto.Page{
